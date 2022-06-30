c5d7cf49a4e0dcc943bcdf5d8598b1a1fc12de50
mas00fr <martin.straesser@uni-wuerzburg.de>
2022-06-30 15:41:40 +0200

    Bump to misim 3.2

--- hello_world.sh
+++ hello_world.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-mvn install:install-file -Dfile=C:\Users\mas00fr\IdeaProjects\misim-orchestration-github\libraries\misim-3.1.jar -DgroupId="cambio.simulator" -DartifactId=misim -Dversion="3.1" -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=C:\Users\mas00fr\IdeaProjects\misim-orchestration-github\libraries\misim-3.2.jar -DgroupId="cambio.simulator" -DartifactId=misim -Dversion="3.2" -Dpackaging=jar -DgeneratePom=true
 
 mvn clean package -B --file pom.xml "-Dmaven.javadoc.skip=true"
 
-java -jar ./target/misim-orchestration.jar -a .\BasicExample/architecture_model.json -e .\BasicExample/example_model.json --orchestration .\BasicExample/orchestration_config.json -d
+java -jar ./target/misim-orchestration.jar -a .\BasicExample/architecture_model.json -e .\BasicExample/example_model.json --orchestration .\BasicExample/orchestration_config.yaml -d
 
